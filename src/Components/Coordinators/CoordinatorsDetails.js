02af91f2a972467cfc9923d7d4404f3429d6cea1
JithuSaaronB <jitusaaron@gmail.com>
2025-03-14 12:12:04 +0530

    feat: update coordinator names and add new entries in CoordinatorsDetails

--- src/Components/Coordinators/CoordinatorsDetails.js
+++ src/Components/Coordinators/CoordinatorsDetails.js
@@ -1,54 +1,70 @@
 import Avatar from '../../assets/Coordinators/avatar.png'
 
 const Coordinators1 = [
-    { name: "Sri Dharshana", image: Avatar },
-    { name: "Elavarasi", image: Avatar },
-    { name: "Nandhana N", image: Avatar },
-    { name: "Aashika K S", image: Avatar },
-    { name: "Poovarasi R", image: Avatar },
-    { name: "Lavanya S", image: Avatar },
-    { name: "Deepajothy", image: Avatar },
-    { name: "Athesh K", image: Avatar },
-    { name: "Abijith ", image: Avatar },
-    { name: "Akilesh", image: Avatar },
-    { name: "Karthic L", image: Avatar },
-    { name: "Dhanushkodi", image: Avatar },
-    { name: "Puspharaj P", image: Avatar },
-    { name: "Soorya R", image: Avatar },
+    { name: "Sri Dharshana", image: Avatar },//yes
+    { name: "Elavarasi", image: Avatar },//yes
+    { name: "Nandhana N", image: Avatar },//yes
+    { name: "Aashika K S", image: Avatar },//yes
+    { name: "Poovarasi R", image: Avatar },//yes
+    { name: "Lavanya S", image: Avatar },//yes
+    { name: "Deepajothy", image: Avatar },//yes
+    { name: "Abijith J", image: Avatar },//yes
+    { name: "Akilesh S", image: Avatar },//yes
+    { name: "Karthic L", image: Avatar },//yes
+    { name: "Dhanushkodi", image: Avatar },//yes
+    { name: "Puspharaj P", image: Avatar },//yes
+    { name: "Soorya R", image: Avatar },//yes
+    { name: "Sangeerthana S", image: Avatar },//yes
+    { name: "Athesh K", image: Avatar },//yes
+    { name: "Bhoomish R", image: Avatar },//yes
+    { name: "Vaitheeshwaran S", image: Avatar },//yes
+    { name: "Logeshwaran L", image: Avatar },//yes
+    { namw: "Anu M", image: Avatar },//yes
+    
 ]
 const Coordinators2 = [
-    { name: "Samson Durai", image: Avatar },
-    { name: "Ajay", image: Avatar },
-    { name: "Sivashankaran", image: Avatar },
-    { name: "Rohith", image: Avatar },
-    { name: "Alphin V T", image: Avatar },
-    { name: "Jithu Saaron B", image: Avatar },
-    { name: "Aswin Raj", image: Avatar },
-    { name: "Syed Aswan", image: Avatar },
-    { name: "Rinesh Guru", image: Avatar },
-    { name: "Ranesh Guru", image: Avatar },
-    { name: "Yeshwanth S R", image: Avatar },
-    { name: "Kishore C", image: Avatar },
-    { name: "Dharshan N", image: Avatar },
-    { name: "Akash M", image: Avatar },
-
+    { name: "Samson Durai S", image: Avatar },//yes
+    { name: "Ajay Vetri N J", image: Avatar },//yes
+    { name: "Sivashankaran R", image: Avatar },//yes
+    { name: "Gokul V M", image: Avatar },//yes
+    { name: "Rohith S", image: Avatar },//yes
+    { name: "Alphin V T", image: Avatar },//yes
+    { name: "Jithu Saaron B", image: Avatar },//yes
+    { name: "Aswin Raj", image: Avatar },//yes
+    { name: "Syed Aswan S A", image: Avatar },//yes
+    { name: "Rinesh Guru S", image: Avatar },//yes
+    { name: "Ranesh Guru S", image: Avatar },//yes
+    { name: "Yeshwanth S R", image: Avatar },//yes
+    { name: "Kishore C", image: Avatar },//yes
+    { name: "Dharshan N", image: Avatar },//yes
+    { name: "Akash M", image: Avatar },//yes
+    { name: "Mouneesh P", image: Avatar },//yes
+    { name: "Yageshwaran N", image: Avatar },//yes
+    { name: "Sham J", image: Avatar },//yes
+    { name: "Meganathan V", image: Avatar },//yes
 
 ]
 const Coordinators3 = [
 
-    { name: "Manimaran B", image: Avatar },
-    { name: "Gagan Kailash R", image: Avatar },
-    { name: "Vijay T", image: Avatar },
-    { name: "Hariharapandian S", image: Avatar },
-    { name: "Valanprakash R", image: Avatar },
-    { name: "Gopinath D", image: Avatar },
-    { name: "Suryaprakash P", image: Avatar },
-    { name: " Vishwa R", image: Avatar },
-    { name: "Ajish E", image: Avatar },
-    { name: "Mouneesh P", image: Avatar },
-    { name: "Dharun S", image: Avatar },
-    { name: "Perarasu S", image: Avatar },
-    { name: "Jeyasingh R", image: Avatar },
-    { name: "Paramesh N", image: Avatar },
+    { name: "Manimaran B", image: Avatar },//yes
+    { name: "Gagan Kailash R", image: Avatar },//yes
+    { name: "Vijay T", image: Avatar },//yes
+    { name: "Hariharapandian S", image: Avatar },//yes
+    { name: "Valanprakash R", image: Avatar },//yes
+    { name: "Gopinath D", image: Avatar },//yes
+    { name: "Suryaprakash P", image: Avatar },//yes
+    { name: " Vishwa R", image: Avatar },//yes
+    { name: "Ajish E", image: Avatar },//yes
+    { name: "Somesh R", image: Avatar },//yes
+    { name: "Dharun S", image: Avatar },//yes
+    { name: "Perarasu S", image: Avatar },//yes
+    { name: "Jeyasingh R", image: Avatar },//yes
+    { name: "Hameshwar", image: Avatar },//yes
+    { name: "Sriram", image: Avatar },//yes
+    { name: "Harivignesh R", image: Avatar },//yes
+    { name: "Prasanth K", image: Avatar },//yes
+    { name: "Sridharan M", image: Avatar },//yes
+    { name: "Senthilmurugan S", image: Avatar },//yes
+   
 ]
 export {Coordinators1,Coordinators2,Coordinators3};
